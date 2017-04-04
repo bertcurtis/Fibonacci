f869be73ba36855140aa32c495f7836be31739c4
bertcurtis <bertc@4life.com>
2017-04-04 11:44:55 -0600

    Add files via upload

---
+++ Fibonccocheese/Fibonccocheese/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Fibonccocheese
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.Font = new System.Drawing.Font("Papyrus", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.textBox1.Location = new System.Drawing.Point(39, 96);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(836, 224);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(224, 335);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(429, 20);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.HelpRequested += new System.Windows.Forms.HelpEventHandler(this.textBox_HelpRequested);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Font = new System.Drawing.Font("Papyrus", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(224, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(429, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click Here\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Enter += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(13, 501);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(191, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Click Here to view an instructive video!";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(612, 501);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(285, 13);
+            this.linkLabel2.TabIndex = 4;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Click here to see how I feel about the Fibonacci sequence!";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AccessibleName = "";
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(909, 526);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fibo-Nacho-Cheese";
+            this.TransparencyKey = System.Drawing.Color.Maroon;
+            //this.Load += new System.EventHandler(this.textBox_HelpRequested);
+            this.HelpRequested += new System.Windows.Forms.HelpEventHandler(this.textBox_HelpRequested);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    
+    }
+}
+
---
+++ Fibonccocheese/Fibonccocheese/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fibonccocheese
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "     When Fibonacci was a young man he was very good at math. But he was too good at it. " +
+                "Everyone mocked him. His mother told him that it was because they were envious of him. " +
+                "He didn't believe her so he developed a formula to see if she was telling the truth or not. " +
+                "The formula is this:" +
+                "Type the first letter of your first name to see how many people are envious of you. " +
+                "When you are done click below or press enter.";
+        }
+
+
+
+        private string fibonacci()
+        {
+            string result = "";
+            int a = 0;
+            int b = 1;
+            int c = 1;
+            int d = b + c;
+            int e = d + c;
+            int f = d + e;
+            int g = f + e;
+            int h = g + f;
+            int i = h + g;
+            int j = i + h;
+            int k = j + i;
+            int l = k + j;
+            int m = l + k;
+            int n = m + l;
+            int o = n + m;
+            int p = o + n;
+            int q = p + o;
+            int r = q + p;
+            int s = r + q;
+            int t = s + r;
+            int u = t + s;
+            int v = u + t;
+            int w = v + u;
+            int x = w + v;
+            int y = x + w;
+            int z = y + x;
+            string userValue7 = textBox2.Text;
+
+            switch (userValue7)
+            {
+                case "a":
+                    result = a.ToString();
+                    break;
+                case "b":
+                    result = b.ToString();
+                    break;
+                case "c":
+                    result = c.ToString();
+                    break;
+                case "d":
+                    result = d.ToString();
+                    break;
+                case "e":
+                    result = e.ToString();
+                    break;
+                case "f":
+                    result = f.ToString();
+                    break;
+                case "g":
+                    result = g.ToString();
+                    break;
+                case "h":
+                    result = h.ToString();
+                    break;
+                case "i":
+                    result = i.ToString();
+                    break;
+                case "j":
+                    result = j.ToString();
+                    break;
+                case "k":
+                    result = k.ToString();
+                    break;
+                case "l":
+                    result = l.ToString();
+                    break;
+                case "m":
+                    result = m.ToString();
+                    break;
+                case "n":
+                    result = n.ToString();
+                    break;
+                case "o":
+                    result = o.ToString();
+                    break;
+                case "p":
+                    result = p.ToString();
+                    break;
+                case "q":
+                    result = q.ToString();
+                    break;
+                case "r":
+                    result = r.ToString();
+                    break;
+                case "s":
+                    result = s.ToString();
+                    break;
+                case "t":
+                    result = t.ToString();
+                    break;
+                case "u":
+                    result = u.ToString();
+                    break;
+                case "v":
+                    result = v.ToString();
+                    break;
+                case "w":
+                    result = w.ToString();
+                    break;
+                case "x":
+                    result = x.ToString();
+                    break;
+                case "y":
+                    result = y.ToString();
+                    break;
+                case "z":
+                    result = z.ToString();
+                    break;
+                //case "done":
+                //result = "".ToString();
+                //break;
+                default:
+                    result = " Fibonacci is growing impatient with your inability to follow simple directions... Absolutely no";
+
+                    break;
+
+
+            }
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fibonacci2 = fibonacci();
+            MessageBox.Show(fibonacci2 + " people are envious of you.");
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=JmvnYnQv1UI");
+            }
+            catch { }
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=ussCHoQttyQ");
+            }
+            catch { }
+        }
+
+
+
+
+        private void textBox_HelpRequested(object sender, System.Windows.Forms.HelpEventArgs hlpevent)
+        {
+
+            string helpBox = "If this box is showing up it means you have clicked something using the '?' button in the top right corner." +
+                " This action is intended to provide" + 
+                " help for those who are having difficulty with this simple program. If you are one of those people then there is" +
+                " help for you. But you will not find it here. This is simply a text box meant to instruct you in regards to this program. Due to the fact that this" +
+                " program is so simple it is obvious to me that if you actually need help navigating navigating this program then you should close the program" +
+                " immediately and seek professional help. Although if you can't work this program then you probably will not be capable to seek professional help" +
+                " in which case I am now unsure how to help you. Good luck in your life endeavors and please don't operate any heavy machinery. Ever.";
+            MessageBox.Show(helpBox);
+        }
+
+     
+
+    }
+
+    }
